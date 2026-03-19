eb1592dbbb7ec5c42671cd459b2f3746706544a3
Anas Sheashaey <she3sha3y5@gmail.com>
2026-03-20 01:19:45 +0200

    refactor(causeeffect.R): rename causeEffectDiagram layout helpers
    redefine:
      - branch_slots <- max(upper_branch_count, lower_branch_count)
      - branch_anchors <- mean2(ac)
    
    rename:
      - a -> branch_spacing
      - a -> intercept
      - nc -> branch_count
      - ncup -> upper_branch_count
      - nclo -> lower_branch_count
      - ncc -> max_subcauses_per_branch
      - we -> effect_label_width
      - wc -> max_subcause_label_width
      - hc -> max_label_height
    
    also added `unused variable` to hc and wc

--- R/causeeffect.R
+++ R/causeeffect.R
@@ -20,10 +20,10 @@ causeEffectDiagram <- function(cause, effect,
     return(m)
   }
   
-  nc <- length(cause)
-  ncup <- nc - round(nc/2)
-  nclo <- nc - ncup
-  ncc <- max(sapply(cause, length))
+  branch_count <- length(cause)
+  upper_branch_count <- branch_count - round(branch_count/2)
+  lower_branch_count <- branch_count - upper_branch_count
+  max_subcauses_per_branch <- max(sapply(cause, length))
 
   plot <- ggplot() +
     xlim(0, 100) + ylim(0, 100) +
@@ -37,45 +37,47 @@ causeEffectDiagram <- function(cause, effect,
   
   size <- cex*12/ggplot2::.pt
   inches_per_unit <- 6 / 100  # ~ 6 inches across 100 units
-  we <- strwidth(effect, units = "inches") /inches_per_unit * 1.1
-  wc <- max(unlist(sapply(cause, strwidth, units = "inches"))) /inches_per_unit
-  hc <- max(strheight(effect, units="inches"),
-            unlist(sapply(cause, strheight, units="inches"))) /inches_per_unit
+  effect_label_width <- strwidth(effect, units = "inches") /inches_per_unit * 1.1
+  max_subcause_label_width <- max(unlist(sapply(cause, strwidth, units = "inches"))) /inches_per_unit # FIX: unused variable
+  max_label_height <- max(strheight(effect, units="inches"),
+            unlist(sapply(cause, strheight, units="inches"))) /inches_per_unit # FIX: unused variable
 
   plot <- plot +  
     # add main spine
-    geom_segment(aes(x = 0, y = 50, xend = 100-we-2, yend = 50),
+    geom_segment(aes(x = 0, y = 50, xend = 100-effect_label_width-2, yend = 50),
                  arrow = arrow(length = unit(0.02, "npc")), 
                  linewidth = 1) +
     # add effect label
-    annotate("text", x = 100-we, y = 50,  
+    annotate("text", x = 100-effect_label_width, y = 50,  
              label = effect, hjust = 0, 
              fontface = font[3], size = size[3])
 
   # draw branches and cause labels
-  a <- (100-we)/(max(ncup,nclo)+1)
-  ac <- a*(0:(max(ncup,nclo)))
+  branch_slots <- max(upper_branch_count, lower_branch_count)
+  branch_spacing <- (100-effect_label_width)/(branch_slots+1)
+  ac <- branch_spacing*(0:branch_slots)
+  branch_anchors <- mean2(ac)
   for(i in 1:(length(ac)-1))
   { 
     plot <- plot +
-      annotate("segment", x = mean2(ac)[i], y = 95, xend = ac[i+1], yend = 50) +
-      annotate("text", x = mean2(ac)[i], y = 97, label = names(cause)[i], 
+      annotate("segment", x = branch_anchors[i], y = 95, xend = ac[i+1], yend = 50) +
+      annotate("text", x = branch_anchors[i], y = 97, label = names(cause)[i], 
                vjust = 0, fontface = font[2], size = size[2])
-    if(i <= nclo)
+    if(i <= lower_branch_count)
     { 
       plot <- plot + 
-        annotate("segment", x = mean2(ac)[i], y = 5, xend = ac[i+1], yend = 50) +
-        annotate("text", x = mean2(ac)[i], y = 3, label = names(cause)[ncup+i], 
+        annotate("segment", x = branch_anchors[i], y = 5, xend = ac[i+1], yend = 50) +
+        annotate("text", x = branch_anchors[i], y = 3, label = names(cause)[upper_branch_count+i], 
                  vjust = 1, fontface = font[2], size = size[2])
     }
   }
   # draw labels for upper branches
-  for (j in 1:ncup)
+  for (j in 1:upper_branch_count)
   { 
-    b <- (50-95)/(ac[j+1]-mean2(ac)[j])
-    a <- 95-b*mean2(ac)[j]
-    y <- rev(50+cumsum((95-50)/(ncc+1))*(1:(ncc)))
-    x <- (y-a)/b
+    b <- (50-95)/(ac[j+1]-branch_anchors[j])
+    intercept <- 95-b*branch_anchors[j]
+    y <- rev(50+cumsum((95-50)/(max_subcauses_per_branch+1))*(1:(max_subcauses_per_branch)))
+    x <- (y-intercept)/b
     for (i in 1:length(y))
     { 
       label <- cause[[j]][i]
@@ -88,17 +90,17 @@ causeEffectDiagram <- function(cause, effect,
     }
   }
   # draw labels for lower branches
-  for (j in 1:ncup)
+  for (j in 1:upper_branch_count)
   { 
-    b <- (50-5)/(ac[j+1]-mean2(ac)[j])
-    a <- 5-b*mean2(ac)[j]
-    y <- cumsum((95-50)/(ncc+1))*(1:(ncc))
-    x <- (y-a)/b
-    if (j <= nclo)
+    b <- (50-5)/(ac[j+1]-branch_anchors[j])
+    intercept <- 5-b*branch_anchors[j]
+    y <- cumsum((95-50)/(max_subcauses_per_branch+1))*(1:(max_subcauses_per_branch))
+    x <- (y-intercept)/b
+    if (j <= lower_branch_count)
     {
       for (i in 1:length(y))
       { 
-        label <- cause[[ncup+j]][i]
+        label <- cause[[upper_branch_count+j]][i]
         if (!is.na(label))
         {
           plot <- plot + 
@@ -111,4 +113,3 @@ causeEffectDiagram <- function(cause, effect,
   
   return(plot)
 }
-
